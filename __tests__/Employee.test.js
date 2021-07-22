4fcd1df37ad73d016e8a4de401d7484d38d35bdf
Meghan Bock <meghan.bock@gmail.com>
2021-07-21 19:20:45 -0500

    setup Employee.js tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,28 @@
+const Employee = require('../lib/Employee');
+const { expect } = require('@jest/globals');
+
+test('creates employee object', () => {
+    const employee = new Employee('Test Employee','Test ID','Test Email');
+
+    expect(employee.name).toBe('Test Employee');
+    expect(employee.id).toBe('Test ID');
+    expect(employee.email).toBe('Test Email');
+});
+
+test('get employee name', () => {
+    const employee = new Employee('Test Employee','Test ID','Test Email');
+
+    expect(employee.getName()).toBe('Test Employee');
+});
+
+test('get employee id', () => {
+    const employee = new Employee('Test Employee','Test ID','Test Email');
+
+    expect(employee.getId()).toBe('Test ID');
+});
+
+test('get employee email', () => {
+    const employee = new Employee('Test Employee','Test ID','Test Email');
+
+    expect(employee.getEmail()).toBe('Test Email');
+});
